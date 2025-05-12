b577e7117d7ff368679502e860de49cf2b6cccda
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2025-05-12 20:36:36 -0300

    Add Aula 05

---
+++ Aula05/script01.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5];
+let nomes = ["Leonardo", "Sofia"];
+let salario = 1000.50;
+
+//console.log("Esses 2 nomes formam um belo casal:", nomes, numbers);
+console.log("Essa pessoa ganha muito mal:", salario, "Reais")
+//console.log("Olá mundo!");
