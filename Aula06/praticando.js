23fafcfa6933a3779e7bbe10cd229229a7713293
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2025-06-02 20:16:32 -0300

    add exercicios

---
+++ Aula06/praticando.js
@@ -0,0 +1,12 @@
+//Exercicio número 1
+let numero = 123;
+let resultado = String(numero);
+
+console.log(resultado);
+
+//Exercicio número 2
+
+let texto = "456"
+let resultado = Number(texto);
+
+//console.log(resultado);
